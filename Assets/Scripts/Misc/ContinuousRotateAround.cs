c38877e89a69cf06ca53140f3ee3a6a679493e47
jake platford <jake.platford18@gmail.com>
2018-02-20 11:27:18 +0000

    Improve asteroids on menu screen so that asteroids rotate around the camera, gives it depth, bit more interesting

---
+++ Assets/Scripts/Misc/ContinuousRotateAround.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinuousRotateAround : MonoBehaviour {
+	public Transform point;
+	public float RotationSpeed = 0f;
+
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround (point.position, new Vector3 (0, 1, 1), RotationSpeed * Time.deltaTime);
+	}
+}
